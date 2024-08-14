e96ceac087a9c796cc88c804aed32f7ac098f348
Thomas Martinez <devtommie114@gmail.com>
2024-08-13 23:41:30 -0400

    started Hero

--- 04-website-projects/00-clipboard/tailwind.config.js
+++ 04-website-projects/00-clipboard/tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         strongCyan: 'hsl(171, 66%, 44%)',
         lightBlue: 'hsl(233, 100%, 69%)',
         darkGrayishBlue: 'hsl(210, 10%, 33%)',
-        grayishBlue: 'hsl(201, 111%, 66%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
       },
       fontFamily: {
         sans: ['Bai Jamjuree', 'sans-serif'],
